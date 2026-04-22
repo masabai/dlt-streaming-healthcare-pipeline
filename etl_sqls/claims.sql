2394ed903f793eab573c271925a8da0d671bb5b0
masabai <winthutaye@gmail.com>
2026-04-21 19:36:35 -0700

    untrack

--- etl_sqls/claims.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE patient_data_governance.testing.claims
-ALTER COLUMN Id SET TAGS ('classification' = 'identifier');
-
-ALTER TABLE patient_data_governance.testing.claims
-ALTER COLUMN PATIENTID SET TAGS ('classification' = 'PII_LINKED');
-
-ALTER TABLE patient_data_governance.testing.claims
-ALTER COLUMN PROVIDERID SET TAGS ('classification' = 'internal_reference');
--- etl_sqls/encounter.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE patient_data_governance.silver.encounters
-ALTER COLUMN Id SET TAGS ('classification' = 'identifier');
-
-ALTER TABLE patient_data_governance.silver.encounters
-ALTER COLUMN START SET TAGS ('classification' = 'timestamp');
-
-ALTER TABLE patient_data_governance.silver.encounters
-ALTER COLUMN STOP SET TAGS ('classification' = 'timestamp');
